93434c8111822009caaab7cb77c5e3f9ee28dadb
Chandan Kumar Saha <chandankumarshah8@gmail.com>
2022-04-15 19:37:22 +0530

    text area debugged

--- src/Notepad.java
+++ src/Notepad.java
@@ -1,3 +1,53 @@
-public class Notepad {
-    
+
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+
+public class Notepad extends JFrame implements ActionListener,WindowListener {
+
+    JTextArea jta=new JTextArea();
+    File fnameContainer;
+
+    public Notepad(){
+        Font fnt=new Font("Arial",Font.PLAIN,15);
+        Container con=getContentPane();
+        JMenuBar jmb=new JMenuBar();
+        JMenu jmfile = new JMenu("File");
+        JMenu jmedit = new JMenu("Edit");
+        JMenu jmhelp = new JMenu("Help");
+
+        con.setLayout(new BorderLayout());
+
+        JScrollPane sbrText = new JScrollPane(jta);
+        sbrText.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        sbrText.setVisible(true);
+
+        jta.setFont(fnt);
+        jta.setLineWrap(true);
+        jta.setWrapStyleWord(true);
+
+        con.add(sbrText);
+
+        createMenuItem(jmfile,"New");
+        createMenuItem(jmfile,"Open");
+        createMenuItem(jmfile,"Save");
+        jmfile.addSeparator();
+        createMenuItem(jmfile,"Exit");
+
+        createMenuItem(jmedit,"Cut");
+        createMenuItem(jmedit,"Copy");
+        createMenuItem(jmedit,"Paste");
+
+        createMenuItem(jmhelp,"About Us");
+
+        jmb.add(jmfile);
+        jmb.add(jmedit);
+        jmb.add(jmhelp);
+
+        setJMenuBar(jmb);
+
+    }
 }
