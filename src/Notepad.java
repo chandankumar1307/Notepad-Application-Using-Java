70241c9bcf009d4e4c065f33819a0cd7e57ed6d9
Chandan Kumar Saha <chandankumarshah8@gmail.com>
2022-04-17 15:26:55 +0530

    project completed

--- src/Notepad.java
+++ src/Notepad.java
@@ -49,5 +49,119 @@ public class Notepad extends JFrame implements ActionListener,WindowListener {
 
         setJMenuBar(jmb);
 
+        setIconImage(Toolkit.getDefaultToolkit().getImage("notepad.gif"));
+        addWindowListener(this);
+        setSize(500,500);
+        setTitle("untitled.txt  -Notepad");
+        setVisible(true);
+
+
+
     }
+
+    public void createMenuItem(JMenu jm,String txt){
+        JMenuItem jmi = new JMenuItem(txt);
+        jmi.addActionListener(this);
+        jm.add(jmi);
+
+    }
+
+    public void actionPerformed(ActionEvent e)
+        {
+            JFileChooser jfc = new JFileChooser();
+            if(e.getActionCommand().equals("New")){
+                this.setTitle("untitled.txt  -Notepad");
+                jta.setText("");
+                fnameContainer=null;
+            }
+            else if(e.getActionCommand().equals("Open")){
+                int ret = jfc.showDialog(null, "Open");
+                if(ret==JFileChooser.APPROVE_OPTION){
+                    try {
+                        File fyl = jfc.getSelectedFile();
+                        OpenFile(fyl.getAbsolutePath());
+                        this.setTitle(fyl.getName()+" - Notepad");
+                        fnameContainer=fyl;
+
+                    } catch (IOException ei) {
+                        
+                    }
+                }
+            }
+            else if(e.getActionCommand().equals("Save")){
+                if(fnameContainer!=null){
+                    jfc.setCurrentDirectory(fnameContainer);
+                    jfc.setSelectedFile(fnameContainer);
+                }else{
+                    jfc.setSelectedFile(new File("Untitled.txt"));
+
+                }
+                int ret=jfc.showDialog(null,"Save");
+                if(ret ==JFileChooser.APPROVE_OPTION){
+                    try {
+                        File fyl = jfc.getSelectedFile();
+                        SaveFile(fyl.getAbsolutePath());
+                        this.setTitle(fyl.getName()+" - Notepad");
+                        fnameContainer = fyl;
+                    } catch (Exception ee) {
+                        
+                    }
+                }
+            }
+            else if(e.getActionCommand().equals("Exit")){
+                Exiting();
+
+            }
+            else if(e.getActionCommand().equals("Copy")){
+                jta.copy();
+            }
+            else if(e.getActionCommand().equals("Paste")){
+                jta.paste();
+            }
+            else if(e.getActionCommand().equals("About Us")){
+                JOptionPane.showMessageDialog(this, "Created by Aman Chauhan and Chandan Kumar","Notepad",JOptionPane.INFORMATION_MESSAGE);
+            }
+            else if(e.getActionCommand().equals("Cut")){
+                jta.cut();
+            }
+        }
+        public void OpenFile(String fname) throws IOException{
+            BufferedReader d = new BufferedReader(new InputStreamReader(new FileInputStream(fname)));
+            String l;
+            jta.setText("");
+            setCursor(new Cursor(Cursor.WAIT_CURSOR));
+            while((l=d.readLine()) !=null){
+                jta.setText("");
+
+            }
+            setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+            d.close();
+        }
+        public void SaveFile(String fname)throws IOException{
+            setCursor(new Cursor(Cursor.WAIT_CURSOR));
+            DataOutputStream o =new DataOutputStream(new FileOutputStream(fname));
+            o.writeBytes(jta.getText());
+            o.close();
+            setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+        }
+
+        public void windowDeactivated(WindowEvent e){
+
+        }
+        public void windowActivated(WindowEvent e){}
+
+        public void windowDeiconified(WindowEvent e){}
+
+        public void windowIconified(WindowEvent e){}
+        public void windowClosed(WindowEvent e){}
+        public void windowClosing(WindowEvent e){
+            Exiting();
+        }
+        public void windowOpened(WindowEvent e){}
+
+        public void Exiting(){
+            System.exit(0);
+        }
+
+
 }
